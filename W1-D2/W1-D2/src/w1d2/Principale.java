f6d44f571d8cf7e1172adb8d5f90b1a83ebf472d
VinceDV <122369775+VinceDV@users.noreply.github.com>
2023-05-03 17:36:33 +0200

    Add files via upload

---
+++ W1-D2/W1-D2/src/w1d2/Principale.java
@@ -0,0 +1,24 @@
+package w1d2;
+
+public class Principale {
+	public static void main (String[] args) {
+		System.out.println("This is my first Epicode Java Project! WOW");
+		
+		Rettangolo r1 = new Rettangolo(2, 5);
+		Rettangolo r2 = new Rettangolo(3, 5);
+
+		r1.stampaRettangolo();
+		r2.stampaRettangolo();
+
+		stampaDueRettangoli(r1, r2);
+	}
+
+	public static void stampaDueRettangoli(Rettangolo x, Rettangolo y) {
+		double perimetri = x.Perimetro() + y.Perimetro();
+		double aree = x.Area() + y.Area();
+		System.out.println(aree);
+		System.out.println(perimetri);
+	}
+
+	
+}
